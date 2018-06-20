c2ac3f94f8e86d66f5920986de1c6e27c3b469a1
Brandon Green <brandon@brandonleeproductions.com>
2018-06-20 01:12:33 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,11 @@ class BankAccount
 
   attr_reader :name # Using reader because I don't want this to change
   attr_accessor :balance, :status # Using accessor because these can change
-  
+
   def initialize(name) # initializes with a name
     @name = name
-    
+    @balance = 0
+    @status = "open"
   end
 
 end
